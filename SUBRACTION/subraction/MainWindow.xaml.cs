0347265cb09b746ab54a2c6a4e23910daaa7a918
renuga <121298131+Renugamanikandan@users.noreply.github.com>
2024-10-13 19:51:21 +0530

    #

---
+++ SUBRACTION/subraction/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace subraction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnresult_Click(object sender, RoutedEventArgs e)
+        {
+            int NUM1 = Convert.ToInt32(txtvalue1.Text);
+            int NUM2 = Convert.ToInt32(txtvalue2.Text);
+            int OUTPUT = NUM1 - NUM2;
+            MessageBox.Show(Convert.ToString(OUTPUT));
+
+        }
+    }
+}
